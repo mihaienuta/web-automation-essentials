ad5205333fc8285565db63247d2f0ceb91ca5de8
mihaienuta <32834539+mihaienuta@users.noreply.github.com>
2017-10-27 14:36:18 +0300

    Create men.js

---
+++ men.js
@@ -0,0 +1,8 @@
+'About Us' : '#v5mrvn-reveal',
+'Carousel Button Next' : 'button.orbit-next'
+'Carousel Button Previous' : 'button.orbit.previous'
+'Our Products' : 'a[href="#"'
+'Product Dropdown 1' : 'ul[role="menu"] li:nth-child(1)'
+'Product Dropdown 2' : 'ul[role="menu"] li:nth-child(2)'
+'Product Dropdown 3' : 'ul[role="menu"] li:nth-child(3)'
+'Product Dropdown 4' : 'ul[role="menu"] li:nth-child(4)'
